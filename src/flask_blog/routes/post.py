57a1df2f2f967930bffac093b51dd92c6c08d97c
YVVV19 <vadimyankovskiy2000@gmail.com>
2024-09-08 11:39:01 +0300

    Co-authored-by: Acuta Pugione <Acutapugione@users.noreply.github.com>

--- src/flask_blog/routes/post.py
+++ src/flask_blog/routes/post.py
@@ -1,25 +1,7 @@
-from sqlalchemy import select
+from sqlalchemy import select, delete
+import asyncio
 from quart import render_template, request, redirect, url_for
 from . import Author, Post, app, Config
-
-# from . import Author, app, Config
-
-
-@app.post("/posts")
-async def create_post():
-    form = await request.form
-    if form:
-        with Config.SESSION.begin() as session:
-            post = Post(
-                **form,
-                author=Author(name="acuta"),
-            )
-            session.add(post)
-    return redirect(url_for("posts"))
-
-
-@app.get("/post_details/<int:index>", defaults={"index": 0})
-async def post_details(index: int): ...
 
 
 @app.get("/posts")
@@ -31,6 +13,7 @@ async def posts():
             "index.html",
             posts=[
                 Post(
+                    id=x.id,
                     created=x.created,
                     content=x.content,
                     author=x.author,
@@ -39,3 +22,45 @@ async def posts():
                 for x in posts
             ],
         )
+
+@app.post("/posts")
+async def create_post():
+    form = await request.form
+    if form:
+        with Config.SESSION.begin() as session:
+            post = Post(
+                **form,
+                author=Author(name="vadim"),
+            )
+            session.add(post)
+    
+    return redirect(url_for("posts"))
+
+
+@app.get("/posts/<int:index>/details")
+async def post_details(index: int):
+    await asyncio.sleep(1)
+
+    return "Hello world"
+
+
+@app.get("/posts/delete/<int:index>")
+async def delete_post_page(index:int):
+    return await render_template("delete_posts.html",index = index) 
+
+@app.post("/posts/delete")
+async def delete_post():
+    form = await request.form
+    post_id = form.get("post_id")
+    if post_id:
+        with Config.SESSION.begin() as session:
+            session.execute(delete(Post).where(Post.id == post_id))
+            # post = session.scalars(select(Post).where(Post.id == post_id)).first()
+            # if post:
+    #         post = Post(
+    #             **form,
+    #             author=Author(name="vadim"),
+    #         )
+    #         session.delete(post)
+    # await asyncio.sleep(1)
+    return redirect(url_for(posts.__name__))
